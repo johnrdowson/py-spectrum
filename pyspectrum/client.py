6ad300529a3bc5074e6d33795a25c17792aadadd
= <=>
2021-04-07 11:21:18 +0100

    Add CSV writer function

--- pyspectrum/client.py
+++ pyspectrum/client.py
@@ -1,4 +1,6 @@
+import csv
 from pyspectrum.mixins.models import SpectrumModelsMixin
+from typing import AnyStr, List, Dict, Optional
 
 __all__ = ["SpectrumClient"]
 
@@ -23,3 +25,36 @@ class SpectrumClient(SpectrumModelsMixin):
         spectrum = SpectrumClient()
         spectrum.mixin(SpectrumModelsMixin)
     """
+
+    @staticmethod
+    def to_csv(
+        datalist: List[Dict],
+        filepath: AnyStr,
+        exclude: Optional[List[str]] = None,
+    ) -> None:
+        """
+        This method will store the given list of dict items to a CSV file.  The
+        CSV column headers will be taken from the first list item keys.  The
+        `exclude` list can be used to omit designated columns from the CSV file,
+        for example ['model_type_id'] would omit the 'model_type_id' column.
+
+        Parameters
+        ----------
+        datalist
+        filepath
+        exclude
+        """
+        fieldnames = datalist[0].keys()
+
+        if exclude:
+            fieldnames = [col for col in fieldnames if col not in exclude]
+            for rec in datalist:
+                for key in exclude:
+                    del rec[key]
+
+        with open(filepath, "w+", newline="") as outfile:
+            csv_wr = csv.DictWriter(
+                outfile, fieldnames=fieldnames, quoting=csv.QUOTE_NONNUMERIC
+            )
+            csv_wr.writeheader()
+            csv_wr.writerows(datalist)
